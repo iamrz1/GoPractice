8b511df686607b03c6c581f1a4bddd2acbeb8e82
iamrz1 <rezoan@appscode.com>
2018-11-07 18:02:40 +0600

    Commit C2 : Added Test for GET

--- goWebAPI/server_test.go
+++ goWebAPI/server_test.go
@@ -13,11 +13,11 @@ import (
 
 func TestPost(t *testing.T) {
 	testBook := Book{ID: "0", Name: "Catch 22", Author: "Joseph Heller", Count: 43}
-	b := new(bytes.Buffer)
-	json.NewEncoder(b).Encode(testBook)
-	res, err := http.Post("http://127.0.0.1:8000/book/0", "application/json; charset=utf-8", b)
+	body := new(bytes.Buffer)
+	json.NewEncoder(body).Encode(testBook)
+	res, err := http.Post("http://127.0.0.1:8000/books/0", "application/json; charset=utf-8", body)
 	if err != nil {
-		t.Error("No response")
+		t.Error("POST failed. No response")
 		return
 	}
 
@@ -26,7 +26,41 @@ func TestPost(t *testing.T) {
 		log.Println("Couldn't post. Status Code = " + strconv.Itoa(sc))
 	}
 
-	var bk []Book
+	var bks []Book
+	//log.Println()
+
+	//fmt.Println()
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
+		return
+	} else {
+		fmt.Println("")
+	}
+	err = json.Unmarshal(data, &bks)
+
+	fmt.Println("POST success. Returned:", bks)
+
+}
+func TestGet(t *testing.T) {
+	//time.Sleep(time.Second * 3)
+	testBook := Book{ID: "0", Name: "Catch 22", Author: "Joseph Heller", Count: 43}
+	body := new(bytes.Buffer)
+	json.NewEncoder(body).Encode(testBook)
+
+	res, err := http.Get("http://127.0.0.1:8000/books")
+	if err != nil {
+		t.Error("GET failed. No response.")
+		return
+	}
+
+	sc := res.StatusCode
+	if sc != 200 {
+		log.Println("Couldn't post. Status Code = " + strconv.Itoa(sc))
+	}
+
+	var bks []Book
 	//log.Println()
 
 	//fmt.Println()
@@ -35,10 +69,10 @@ func TestPost(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	} else {
-		//fmt.Println(string(data))
+		fmt.Println("")
 	}
-	err = json.Unmarshal(data, &bk)
+	err = json.Unmarshal(data, &bks)
 
-	fmt.Println(bk)
+	fmt.Println("GET success. Returned:", bks)
 
 }
